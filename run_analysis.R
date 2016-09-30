d0cc94a7d7a44e4f9bfdf340f2d0acb0bcaed0a1
Sherrod Blankner <mooncalf@Sherrods-MacBook-Pro.local>
2016-09-29 22:48:52 -0700

    loaded training data

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(plyr)
 
+
+
 #explanation of dataset
 #http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
@@ -19,12 +21,46 @@ uci_dir <- "/Users/mooncalf/Dropbox/skb/coursera/UCI_HAR_Dataset/"
 test_dir <- "/Users/mooncalf/Dropbox/skb/coursera/UCI_HAR_Dataset/test/"
 train_dir <- "/Users/mooncalf/Dropbox/skb/coursera/UCI_HAR_Dataset/train/"
 
+# function to read in similar two column text files
+readTwoColText <- function(fileName){
+  rawData <- readLines(paste0(uci_dir, fileName))
+  rawData <- simplify2array(strsplit(rawData, split=" "))
+  df <- data_frame(id=as.numeric(rawData[1,]),description<-rawData[2,])
+  return(df)
+}
+
 # explore data
-# read in activities
-actRaw <- readLines(paste0(uci_dir, "activity_labels.txt"))
-actRaw <- simplify2array(strsplit(actRaw, split=" "))
-activities <- data_frame(id=as.numeric(actRaw[1,]),description<-actRaw[2,])
+# read in activities and features
+activities <- readTwoColText("activity_labels.txt")
+head(activities)
+features <- readTwoColText("features.txt")
+head(features)
+
+readOneColText <- function(fileName, dir){
+  rawData <- readLines(paste0(dir, fileName))
+  df <- data_frame(id=1:length(rawData),data=as.character(rawData))
+  return(df)  
+  
+}
+
+#tried something fancier but it didn't work.
+# testFiles <- c("subject_test.txt", "X_test.txt", "y_test.txt")
+# testVars <- c("testSubjects", "testX", "testY")
+
+testSubjects <- readOneColText("subject_test.txt", test_dir)
+str(testSubjects)
+testX <- readOneColText("X_test.txt", test_dir)
+str(testX)
+testY <- readOneColText("y_test.txt", test_dir)
+str(testY)
 
 
+trainSubjects <- readOneColText("subject_train.txt", train_dir)
+str(trainSubjects)
+trainX <- readOneColText("X_train.txt", train_dir)
+str(trainX)
+trainY <- readOneColText("y_train.txt", train_dir)
+str(trainY)
 
-
+#use this to clear the global environment when R slows down
+#rm(list = ls())
