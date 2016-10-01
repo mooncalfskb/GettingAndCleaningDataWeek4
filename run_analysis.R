712d90ce6dc5f1192ea3c7204e2b449d0a771e0a
Sherrod Blankner <mooncalf@Sherrods-MacBook-Pro.local>
2016-09-30 18:32:48 -0700

    trying to figure out sapply

--- run_analysis.R
+++ run_analysis.R
@@ -21,6 +21,7 @@ uci_dir <- "/Users/mooncalf/Dropbox/skb/coursera/UCI_HAR_Dataset/"
 test_dir <- "/Users/mooncalf/Dropbox/skb/coursera/UCI_HAR_Dataset/test/"
 train_dir <- "/Users/mooncalf/Dropbox/skb/coursera/UCI_HAR_Dataset/train/"
 
+
 # function to read in similar two column text files
 readTwoColText <- function(fileName){
   rawData <- readLines(paste0(uci_dir, fileName))
@@ -36,31 +37,52 @@ head(activities)
 features <- readTwoColText("features.txt")
 head(features)
 
-readOneColText <- function(fileName, dir){
+# function to read in basics and number them with ids.
+readOneColText <- function(fileName, dir, start_id){
   rawData <- readLines(paste0(dir, fileName))
-  df <- data_frame(id=1:length(rawData),data=as.character(rawData))
+  end_id <- start_id + as.numeric(length(rawData)) - 1
+  df <- data_frame(id=start_id:end_id,data=as.character(rawData))
   return(df)  
-  
 }
 
 #tried something fancier but it didn't work.
 # testFiles <- c("subject_test.txt", "X_test.txt", "y_test.txt")
 # testVars <- c("testSubjects", "testX", "testY")
 
-testSubjects <- readOneColText("subject_test.txt", test_dir)
+testSubjects <- readOneColText("subject_test.txt", test_dir, 1)
 str(testSubjects)
-testX <- readOneColText("X_test.txt", test_dir)
+testX <- readOneColText("X_test.txt", test_dir, 1)
 str(testX)
-testY <- readOneColText("y_test.txt", test_dir)
+testY <- readOneColText("y_test.txt", test_dir, 1)
 str(testY)
 
-
-trainSubjects <- readOneColText("subject_train.txt", train_dir)
+# start id's of next group at 1 + length of last group
+new_start_id <- as.numeric(length(testSubjects$data)) + 1
+trainSubjects <- readOneColText("subject_train.txt", train_dir, new_start_id)
 str(trainSubjects)
-trainX <- readOneColText("X_train.txt", train_dir)
+trainX <- readOneColText("X_train.txt", train_dir, new_start_id)
 str(trainX)
-trainY <- readOneColText("y_train.txt", train_dir)
+trainY <- readOneColText("y_train.txt", train_dir, new_start_id)
 str(trainY)
+
+testdf <- data_frame()
+
+parseData <- function(fileName, dir, testdf){
+  rawData <- readLines(paste0(dir, fileName, ".txt"))
+  rawData <- simplify2array(strsplit(rawData, split="  "))
+  print(head(rawData))
+  testdf[,fileName] <- rawData
+}
+
+test_data_dir <- "/Users/mooncalf/Dropbox/skb/coursera/UCI_HAR_Dataset/test/Inertial\ Signals/"
+train_data_dir <- "/Users/mooncalf/Dropbox/skb/coursera/UCI_HAR_Dataset/train/Inertial\ Signals/"
+
+test_data_files <- list.files(test_data_dir)
+test_data_files <- simplify2array(strsplit(test_data_files, split=".txt"))
+
+
+sapply(test_data_files, FUN=parseData, dir=test_data_dir)
+
 
 #use this to clear the global environment when R slows down
 #rm(list = ls())
